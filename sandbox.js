971ea22e0a0f87648f4942511ae2ed2a0f32e7a5
Adam L <luzsiadam@gmail.com>
2022-02-09 11:46:14 +0100

    + Sandbox inint

---
+++ sandbox.js
@@ -0,0 +1,17 @@
+class App {
+
+    constructor(jujj) {
+        console.log(jujj);
+    }
+
+}
+
+const qqq = new App('qwe');
+
+
+const obj = {
+    joska: 'pista',
+    ede: `ika ' Mika " Fika`
+}
+
+console.log(obj);
